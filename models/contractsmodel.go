896699a08cb8e27e0e23592cf10406f08434017f
GathangaElijah <elyg3672@gmail.com>
2024-10-23 17:21:38 +0300

    fear: Created data structure to hold contracts information

---
+++ models/contractsmodel.go
@@ -0,0 +1,24 @@
+package models
+
+type Contract struct{
+	ContractNumber string
+	Amount string
+	AwardDate string
+	TenderTitle string
+	EvalCompletionDate string
+	NotificationOfAwardDate string
+	SignDate string
+	StartDate string
+	EndDate string
+	AgpoCertificateNumber string
+	AwardedAgpoGroupId string
+	CreatedBy string
+	Terminated string
+	FinancialYear string
+	Quater string
+	TenderRef string
+	PEName string
+	SupplierName string
+	NoOfBOI string
+	CreatedAt string
+}
